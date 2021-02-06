8f43e24a33adb2c1c2636cb378fd975097943b47
Fionera <fionera@fionera.de>
2021-02-06 21:51:30 +0100

    fix(authgw): Remove ExpiresAt Field (#61)

--- pkg/authgw/db.go
+++ pkg/authgw/db.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"time"
 
 	"go.od2.network/hive/pkg/token"
 )
@@ -16,18 +15,16 @@ type Database struct {
 
 // LookupToken reads a token from SQL.
 func (d *Database) LookupToken(ctx context.Context, id token.ID) (*TokenInfo, error) {
-	const query = "SELECT worker_id, expires_at FROM auth_tokens WHERE id = ?;"
+	const query = "SELECT worker_id FROM auth_tokens WHERE id = ?;"
 	row := d.DB.QueryRowContext(ctx, query, id[:])
 	var workerID int64
-	var expiresAt time.Time
-	if err := row.Scan(&workerID, &expiresAt); errors.Is(err, sql.ErrNoRows) {
+	if err := row.Scan(&workerID); errors.Is(err, sql.ErrNoRows) {
 		return nil, ErrUnknown
 	} else if err != nil {
 		return nil, err
 	}
 	return &TokenInfo{
-		ExpiresAt: expiresAt,
-		Valid:     time.Now().Before(expiresAt),
-		WorkerID:  workerID,
+		Valid:    true,
+		WorkerID: workerID,
 	}, nil
 }
