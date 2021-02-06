a21327b13cb6280babbbe98a4b412397bfadc999
Fionera <fionera@fionera.de>
2021-02-06 21:50:02 +0100

    fix(authgw): Remove ExpiresAt Field

--- pkg/authgw/authgw.go
+++ pkg/authgw/authgw.go
@@ -3,7 +3,6 @@ package authgw
 import (
 	"context"
 	"errors"
-	"time"
 
 	"go.od2.network/hive/pkg/token"
 )
@@ -15,9 +14,8 @@ type Backend interface {
 
 // TokenInfo specifies whether a token authorizes the client for the required service.
 type TokenInfo struct {
-	ExpiresAt time.Time // the time when the token becomes invalid
-	Valid     bool      // whether the last lookup granted authorization (from Policy)
-	WorkerID  int64
+	Valid    bool // whether the last lookup granted authorization (from Policy)
+	WorkerID int64
 }
 
 // ErrUnknown is returned when a token is not present in the DB.
